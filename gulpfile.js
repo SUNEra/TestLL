f51673f659c028c1df13677517acb60a5451f1e4
SUNEra <qxiecanq@qq.com>
2016-04-26 18:20:45 +0800

    gulp and bower

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+var gulp = require('gulp');
 var elixir = require('laravel-elixir');
 
 /*
@@ -10,7 +11,36 @@ var elixir = require('laravel-elixir');
  | file for our application, as well as publishing vendor resources.
  |
  */
+/**
+ * 拷贝任何需要的文件
+ *
+ * Do a 'gulp copyfiles' after bower updates
+ */
+gulp.task("copyfiles", function() {
+
+    gulp.src("vendor/bower_dl/jquery/dist/jquery.js")
+        .pipe(gulp.dest("resources/assets/js/"));
+
+    gulp.src("vendor/bower_dl/bootstrap/less/**")
+        .pipe(gulp.dest("resources/assets/less/bootstrap"));
+
+    gulp.src("vendor/bower_dl/bootstrap/dist/js/bootstrap.js")
+        .pipe(gulp.dest("resources/assets/js/"));
+
+    gulp.src("vendor/bower_dl/bootstrap/dist/fonts/**")
+        .pipe(gulp.dest("public/assets/fonts"));
+
+});
 
 elixir(function(mix) {
+    // 合并 scripts
+    mix.scripts(['js/jquery.js','js/bootstrap.js'],
+        'public/assets/js/admin.js',
+        'resources/assets'
+    );
+
+    // 编译 Less
+    mix.less('admin.less', 'public/assets/css/admin.css');
+
     mix.sass('app.scss');
 });
